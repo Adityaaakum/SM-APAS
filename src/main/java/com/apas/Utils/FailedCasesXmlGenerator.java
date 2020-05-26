156d9340da785a66cc61d366fb771f7ca5ed34d9
nikjain2 <nikhil.jain2@publicissapient.com>
2020-05-26 17:03:58 +0530

    Code refactoring in few files before merging Disabled Veteran Code

--- src/main/java/com/apas/Utils/FailedCasesXmlGenerator.java
+++ src/main/java/com/apas/Utils/FailedCasesXmlGenerator.java
@@ -1,4 +1,4 @@
-package com.apas.FailedCasesExecution;
+package com.apas.Utils;
 
 import java.io.File;
 import java.io.IOException;
--- src/main/java/com/apas/Utils/encryptPassword.java
+++
@@ -1,9 +0,0 @@
-package com.apas.Utils;
-
-public class encryptPassword {
-
-	public static void main(String[] args) {
-		PasswordUtils.encrypt("Support@123", "");
-		System.out.println(PasswordUtils.encrypt("Support@123", ""));
-	}
-}
