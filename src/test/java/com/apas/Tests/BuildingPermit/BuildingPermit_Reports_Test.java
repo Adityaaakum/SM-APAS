140b0a34a8b8e8bc455b932d3d74cb6db766fffa
nikjain2 <nikhil.jain2@publicissapient.com>
2020-06-18 15:28:54 +0530

    regression fixed and code push for SMAB-T549,SMAB-T623

--- src/test/java/com/apas/Tests/BuildingPermit/BuildingPermit_Reports_Test.java
+++ src/test/java/com/apas/Tests/BuildingPermit/BuildingPermit_Reports_Test.java
@@ -1,7 +1,8 @@
-/*package com.apas.Tests.ApasSettings;
+package com.apas.Tests.BuildingPermit;
 
 import com.apas.Assertions.SoftAssertion;
 import com.apas.BrowserDriver.BrowserDriver;
+import com.apas.DataProviders.DataProviders;
 import com.apas.PageObjects.ApasGenericPage;
 import com.apas.PageObjects.Page;
 import com.apas.PageObjects.ReportsPage;
@@ -21,7 +22,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Objects;
 
-public class ReportsTest extends TestBase {
+public class BuildingPermit_Reports_Test extends TestBase {
 
 	private RemoteWebDriver driver;
 	Page objPage;
@@ -30,7 +31,7 @@ public class ReportsTest extends TestBase {
 	ReportsPage objReportsPage;
 	SoftAssertion softAssert  = new SoftAssertion();
 
-	@BeforeMethod
+	@BeforeMethod(alwaysRun=true)
 	public void beforeMethod(){
 		driver = BrowserDriver.getBrowserInstance();
 		objPage = new Page(driver);
@@ -38,27 +39,11 @@ public class ReportsTest extends TestBase {
 		objReportsPage = new ReportsPage(driver);
 		objApasGenericFunctions = new ApasGenericFunctions(driver);
 	}
-		
-	@AfterMethod
-	public void afterMethod() throws IOException, InterruptedException{
-		objApasGenericFunctions.logout();
-		softAssert.assertAll();
-	}
 
-	*//**
-	 * Below function will be used to login to application with different users
-	 *
-	 * @return Return the user business admin and appraisal support in an array
-	 **//*
-	@DataProvider(name = "loginUsers")
-	public Object[][] dataProviderLoginUserMethod() {
-		return new Object[][] {{ users.BUSINESS_ADMIN } };
-	}
-
-	*//**
+	/*
 	 Below test case will validate that user is able to export the report
-	 **//*
-	@Test(description = "SMAB-T433: Validation for Reports Export", dataProvider = "loginUsers", groups = {"smoke","regression"}, priority = 0, alwaysRun = true)
+	 */
+	@Test(description = "SMAB-T433: Validation for Reports Export", dataProvider = "loginBPPBusinessAdmin",dataProviderClass = DataProviders.class, groups = {"smoke","regression"},alwaysRun = false)
 	public void verifyReportExport(String loginUser) throws Exception {
 
 		String downloadLocation = CONFIG.getProperty("downloadFolder");
@@ -66,12 +51,10 @@ public class ReportsTest extends TestBase {
 		String exportedFileName;
 		System.out.println("Download location : " + downloadLocation);
 
-		//Step1: Login to the APAS application using the credentials passed through data provider (Business admin or appraisal support)
-		ExtentTestManager.getTest().log(LogStatus.INFO, "Executing the tests case with user : " + loginUser);
+		//Step1: Login to the APAS application using the credentials passed through data provider
 		objApasGenericFunctions.login(loginUser);
 
-		//Step2: Opening the Non Relevant Permit Settings module
-		ExtentTestManager.getTest().log(LogStatus.INFO, "Opening Reports module");
+		//Step2: Opening the Reports module
 		objApasGenericFunctions.searchModule(modules.REPORTS);
 
 		//Deleteing all the previously downloaded files
@@ -80,7 +63,8 @@ public class ReportsTest extends TestBase {
 		//Step3: Exporting 'Building Permit by City Code' report in Formatted Mode
 		objReportsPage.exportReport(reportName,ReportsPage.FORMATTED_EXPORT);
 		exportedFileName = Objects.requireNonNull(new File(downloadLocation).listFiles())[0].getName();
-		softAssert.assertTrue(exportedFileName.contains("Building Permit by City Code"), "SMAB-T433: Exported report name validation");
+		softAssert.assertTrue(exportedFileName.contains("Building Permit by City Code"), "SMAB-T433: Exported report name validation. Report name should contain 'Building Permit by City Code' in formatted export");
+		softAssert.assertTrue(exportedFileName.endsWith(".xls"), "SMAB-T433: Exported data fomratted report should be in Excel");
 
 		//Deleteing all the previously downloaded files
 		objApasGenericFunctions.deleteFilesFromFolder(downloadLocation);
@@ -88,7 +72,7 @@ public class ReportsTest extends TestBase {
 		//Step3: Exporting 'Building Permit by City Code' report in Data Mode
 		objReportsPage.exportReport(reportName,ReportsPage.DATA_EXPORT);
 		exportedFileName = Objects.requireNonNull(new File(downloadLocation).listFiles())[0].getName();
-		softAssert.assertTrue(exportedFileName.contains("report"), "SMAB-T433: Exported report name validation");
+		softAssert.assertTrue(exportedFileName.contains("report"), "SMAB-T433: Exported report name validation. Report name should contain the word 'Report' in case of data format");
+		softAssert.assertTrue(exportedFileName.endsWith(".csv"), "SMAB-T433: Exported data fomratted report should be in CSV");
 	}
 }
-*/
