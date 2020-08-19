7ac28390f3939042495e093cb1ea485ef18aa6d7
yogsingh5 <yogender.singh@publicissapient.com>
2020-08-19 15:42:32 +0530

    updated EFile fixes for regression

--- src/test/java/com/apas/Tests/BPPTrends/BPPTrend_EfileImport_Test.java
+++ src/test/java/com/apas/Tests/BPPTrends/BPPTrend_EfileImport_Test.java
@@ -1179,7 +1179,7 @@ public class BPPTrend_EfileImport_Test extends TestBase {
 		String period = "2021";
 		String fileType="BPP Trend Factors";
 		String source="BOE - Valuation Factors";
-		String boebppTrendIndexFactorsFile=System.getProperty("user.dir") + testdata.BPP_TREND_BOE_VALUATION_FACTORS_VALID_DATA;
+		String boebppTrendIndexFactorsFile=System.getProperty("user.dir") + testdata.BPP_TREND_BOE_VALUATION_FACTORS_VALID_DATA+"BOE Valuation Factors 2021.xlsx";
 		objApasGenericFunctions.login(loginUser);
 
 		//Step1: Delete the existing data from system before importing files
@@ -1251,7 +1251,7 @@ public class BPPTrend_EfileImport_Test extends TestBase {
 		String period = "2021";
 		String fileType="BPP Trend Factors";
 		String source="BOE - Valuation Factors";
-		String boebppTrendIndexFactorsFile=System.getProperty("user.dir") + testdata.BPP_TREND_BOE_VALUATION_FACTORS;
+		String boebppTrendIndexFactorsFile=System.getProperty("user.dir") + testdata.BPP_TREND_BOE_VALUATION_FACTORS+"BOE Valuation Factors 2021.xlsx";
 		String boeTableErrorRecords=System.getProperty("user.dir") + testdata.BOE_ERRORREOCRDS_COUNT;
 		Map<String, String> errorRecordsCount = objUtil.generateMapFromJsonFile(boeTableErrorRecords, "ErrorRecordsTableWise");
 		
