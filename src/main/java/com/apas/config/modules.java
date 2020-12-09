a1b18b3e307bd796a761f9a5d3d71d8c9dd6a36d
Chirag Arora <CArora@smcgov.org>
2020-12-09 14:14:29 +0530

    Added the code for SMAB-5642 automation coverage - 12/9

--- src/main/java/com/apas/config/modules.java
+++ src/main/java/com/apas/config/modules.java
@@ -3,23 +3,23 @@ package com.apas.config;
 public interface modules {
 	
 	//List of all the modules on APAS application
-	public static final String EFILE_INTAKE = "E-File Import Tool";
-	public static final String EFILE_INTAKE_VIEW = "E-File Intake";
-	public static final String EFILE_IMPORT_TRANSACTIONS = "E-File Import Transactions";
-	public static final String EFILE_IMPORT_LOGS = "E-File Import Logs";
-	public static final String BUILDING_PERMITS = "Building Permits";
-	public static final String BPP_TRENDS = "BPP Trend";
-	public static final String BPP_TRENDS_SETUP = "BPP Trend Setup";
-	public static final String NON_RELEVANT_PERMIT_SETTINGS = "Non Relevant Permit Settings";
-	public static final String CITY_STRAT_CODES = "City Strat Codes";
-	public static final String COUNTY_STRAT_CODES = "County Strat Codes";
-	public static final String REPORTS = "Reports";
-	public static final String PARCELS = "Parcels";
-	public static final String HOME = "Home";
-	public static final String BPP_ACCOUNTS = "BPP Accounts";
-	public static final String CPI_FACTORS = "CPI Factors";
-	public static final String WORK_POOL = "Work Pool";
-	public static final String WORK_ITEM = "Work Item";
+	String EFILE_INTAKE = "E-File Import Tool";
+	String EFILE_INTAKE_VIEW = "E-File Intake";
+	String EFILE_IMPORT_TRANSACTIONS = "E-File Import Transactions";
+	String EFILE_IMPORT_LOGS = "E-File Import Logs";
+	String BUILDING_PERMITS = "Building Permits";
+	String BPP_TRENDS = "BPP Trend";
+	String BPP_TRENDS_SETUP = "BPP Trend Setup";
+	String NON_RELEVANT_PERMIT_SETTINGS = "Non Relevant Permit Settings";
+	String CITY_STRAT_CODES = "City Strat Codes";
+	String COUNTY_STRAT_CODES = "County Strat Codes";
+	String REPORTS = "Reports";
+	String PARCELS = "Parcels";
+	String HOME = "Home";
+	String BPP_ACCOUNTS = "BPP Accounts";
+	String CPI_FACTORS = "CPI Factors";
+	String WORK_POOL = "Work Pool";
+	String WORK_ITEM = "Work Item";
 
 	String REAL_PROPERTY_SETTINGS_LIBRARIES = "Real Property Settings Libraries";
 	String ROLL_YEAR_SETTINGS = "Roll Year Settings";
