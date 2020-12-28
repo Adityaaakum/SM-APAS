3062c4e911daa95eb75f5c519d26aea520ca0ada
Amit Kaila <AKaila@smcgov.org>
2020-12-28 16:14:29 +0530

    pull changes from pre-release

--- src/main/java/com/apas/PageObjects/ParcelsPage.java
+++ src/main/java/com/apas/PageObjects/ParcelsPage.java
@@ -11,6 +11,8 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Map;
 
 public class ParcelsPage extends ApasGenericPage {
@@ -25,7 +27,6 @@ public class ParcelsPage extends ApasGenericPage {
 
 	public String componentActionsButtonText = "Component Actions";
 	public String nextButtonComponentsActionsModal = "Next";
-
 	public String workItemTypeDropDownComponentsActionsModal = "Work Item Type";
 	public String referenceInputTextBoxComponentActionModal = "Reference";
 	public String descriptionInputTextBoxComponentActionModal = "Description";
@@ -36,6 +37,7 @@ public class ParcelsPage extends ApasGenericPage {
 	public String priorityDropDownComponentsActionsModal = "Priority";
 	public String workItemRoutingDropDownComponentsActionsModal = "Work Item Routing";
 	public String workItemOwnerSearchBox = "Work Item Owner (if someone other than you)";
+	public String NeighborhoodCode = "Neighborhood Code";
 
 	@FindBy(xpath = "//p[text()='Primary Situs']/../..//force-hoverable-link")
 	public WebElement linkPrimarySitus;
@@ -68,7 +70,46 @@ public class ParcelsPage extends ApasGenericPage {
 	 * @param dataMap: A data map which contains data to create work item
 	 * @throws Exception
 	 */
-	public String createWorkItem(Map<String, String> dataMap) throws Exception {
+	
+	  public String createWorkItem(Map<String, String> dataMap) throws Exception {
+	  String workItemType = dataMap.get("Work Item Type"); String actions =
+	  dataMap.get("Actions"); String reference = dataMap.get("Reference"); String
+	  description = dataMap.get("Description"); String priority =
+	  dataMap.get("Priority"); String workItemRouting =
+	  dataMap.get("Work Item Routing"); String dueDate = dataMap.get("Due Date");
+	  String dov = dataMap.get("DOV"); String workItemOwner=
+	  dataMap.get("Work Item Owner"); String workItemNumber;
+	  
+	  Click(getButtonWithText(componentActionsButtonText));
+	  waitForElementToBeClickable(selectOptionDropDownComponentsActionsModal);
+	  selectOptionFromDropDown(selectOptionDropDownComponentsActionsModal,
+	  "Create Work Item");
+	  Click(getButtonWithText(nextButtonComponentsActionsModal));
+	  waitForElementToBeClickable(workItemTypeDropDownComponentsActionsModal);
+	  selectOptionFromDropDown(workItemTypeDropDownComponentsActionsModal,
+	  workItemType); selectOptionFromDropDown(actionsDropDownLabel, actions);
+	  enter(referenceInputTextBoxComponentActionModal, reference);
+	  enter(descriptionInputTextBoxComponentActionModal, description);
+	  selectOptionFromDropDown(priorityDropDownComponentsActionsModal, priority);
+	  selectOptionFromDropDown(workItemRoutingDropDownComponentsActionsModal,
+	  workItemRouting); if (dueDate != null) enter(dueDateInputTextBox, dueDate);
+	  if (dov != null) enter(dovInputTextBox, dov); if (workItemOwner != null)
+	  searchAndSelectFromDropDown(workItemOwnerSearchBox,workItemOwner);
+	  Click(getButtonWithText(nextButtonComponentsActionsModal));
+	  
+	  Thread.sleep(2000);
+	  
+	  String workItemQuery = "SELECT Name FROM Work_Item__c where type__C = '" +
+	  workItemType + "' and sub_type__c='" + actions + "' and Reference__C = '" +
+	  reference + "' order by Name desc limit 1"; workItemNumber =
+	  objSalesforceAPI.select(workItemQuery).get("Name").get(0);
+	  ReportLogger.INFO("Work item created is " + workItemNumber );
+	  
+	  return workItemNumber; }
+	 
+		
+	public String ParcelComponentActions(String APN, Map<String, String> dataMap) throws Exception {
+		
 		String workItemType = dataMap.get("Work Item Type");
 		String actions = dataMap.get("Actions");
 		String reference = dataMap.get("Reference");
@@ -78,14 +119,18 @@ public class ParcelsPage extends ApasGenericPage {
 		String dueDate = dataMap.get("Due Date");
 		String dov = dataMap.get("DOV");
 		String workItemOwner= dataMap.get("Work Item Owner");
-		String workItemNumber;
+		String workItemNumber=null;
+		
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy-HH-mm-ss");
+		String upDate = sdf.format(date);
+		reference = reference+upDate;
 
 		Click(getButtonWithText(componentActionsButtonText));
 		waitForElementToBeClickable(selectOptionDropDownComponentsActionsModal);
 		selectOptionFromDropDown(selectOptionDropDownComponentsActionsModal, "Create Work Item");
 		Click(getButtonWithText(nextButtonComponentsActionsModal));
 		waitForElementToBeClickable(workItemTypeDropDownComponentsActionsModal);
-
 		selectOptionFromDropDown(workItemTypeDropDownComponentsActionsModal, workItemType);
 		selectOptionFromDropDown(actionsDropDownLabel, actions);
 		enter(referenceInputTextBoxComponentActionModal, reference);
@@ -96,13 +141,20 @@ public class ParcelsPage extends ApasGenericPage {
 		if (dov != null) enter(dovInputTextBox, dov);
 		if (workItemOwner != null) searchAndSelectFromDropDown(workItemOwnerSearchBox,workItemOwner);
 		Click(getButtonWithText(nextButtonComponentsActionsModal));
-
+		
 		Thread.sleep(2000);
 
-		String workItemQuery = "SELECT Name FROM Work_Item__c where type__C = '" + workItemType + "' and sub_type__c='" + actions + "' and Reference__C = '" + reference + "' order by Name desc limit 1";
-		workItemNumber = objSalesforceAPI.select(workItemQuery).get("Name").get(0);
+		String workItemQuery = "SELECT work_item__r.Name , work_item__r.Reference__c from work_item_linkage__c "
+				+ "where parcel__r.Name = '"+APN+"'"
+				+ "and work_item__r.type__c = '" + workItemType + "' "
+				+ "and work_item__r.sub_type__c = '"+ actions +"'"
+				+ "and work_item__r.reference__c = '"+ reference +"'"
+				+ "order by createddate desc limit 1";
+		
+		workItemNumber = objSalesforceAPI.select(workItemQuery).get("work_item__c.Name").get(0);
 		ReportLogger.INFO("Work item created is " + workItemNumber  );
 		
 		return workItemNumber;
+
 	}
 }
