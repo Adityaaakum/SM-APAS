b2cd32b3a571cb71c9634900637b2363aec57192
VSharma <VSharma@EC2AMAZ-M4CA5DC.smcare.org>
2021-01-12 15:05:06 +0530

    create test case for WI reports.

--- src/test/java/com/apas/Tests/WorkItemsTest/WorkItems_report_Test.java
+++ src/test/java/com/apas/Tests/WorkItemsTest/WorkItems_report_Test.java
@@ -40,15 +40,13 @@ public class WorkItems_report_Test extends TestBase{
 		objApasGenericPage = new ApasGenericPage(driver);
 		objReportsPage = new ReportsPage(driver);
 	}
-	@Test(description = "SMAB-T1783: Validation the Work Item Type and Action based on work item configuration for BPP Accounts", dataProvider = "loginBPPBusinessAdmin", dataProviderClass = DataProviders.class, groups = {"regression", "Work_Items_Manual"}, alwaysRun = true)
+	@Test(description = "SMAB-T2520: Validation the Work Item Type and Action based on work item configuration for BPP Accounts", dataProvider = "loginBPPBusinessAdmin", dataProviderClass = DataProviders.class, groups = {"regression", "Work_Items_Manual"}, alwaysRun = true)
     public void WorkItems_ExecutiveViewReports(String loginUser) throws Exception {
 		String downloadLocation = testdata.DOWNLOAD_FOLDER;
 		String reportName = "Executive All Work Items";
 		String exportedFileName;
 		ReportLogger.INFO("Download location : " + downloadLocation);
-		String workItemCreationData = System.getProperty("user.dir") +testdata.MANUAL_WORK_ITEMS; 
- 		  Map<String, String> hashMapmanualWorkItemData =objUtil.generateMapFromJsonFile(workItemCreationData,"DataToCreateWorkItemOfTypeRP");
- 		  
+		
         // Step1: Login to the APAS application using the credentials passed through data provider (BPP Business Admin)
 		objApasGenericPage.login(loginUser);
         // Step2: Opening the BPP Account page and searching a BPP Account
@@ -57,22 +55,19 @@ public class WorkItems_report_Test extends TestBase{
 		//Deleteing all the previously downloaded files
 		objApasGenericPage.deleteFilesFromFolder(downloadLocation);
 
-		//Step3: Exporting 'Building Permit by City Code' report in Formatted Mode
+		//Step3: Exporting 'Executive All Work Items' report in Formatted Mode
 		objReportsPage.exportReport(reportName,ReportsPage.FORMATTED_EXPORT);
 		File downloadedFile = Objects.requireNonNull(new File(downloadLocation).listFiles())[0];
 		exportedFileName = downloadedFile.getName();
-		softAssert.assertTrue(exportedFileName.contains("Executive All Work Items"), "SMAB-T433,SMAB-T373: Exported report name validation. Report name should contain 'Building Permit by City Code' in formatted export. Actual Report Name : " + exportedFileName);
-		softAssert.assertEquals(exportedFileName.split("\\.")[1],"xlsx", "SMAB-T433,SMAB-T373: Exported data formatted report should be in XLSX");
+		softAssert.assertTrue(exportedFileName.contains("Executive All Work Items"), "SMAB-T2520: Exported report name validation. Report name should contain 'Executive All Work Items' in formatted export. Actual Report Name : " + exportedFileName);
+		softAssert.assertEquals(exportedFileName.split("\\.")[1],"xlsx", "SMAB-T2520: Exported data formatted report should be in XLSX");
 
 		//Step4: Columns validation in exported report
 		HashMap<String, ArrayList<String>> hashMapExcelData = ExcelUtils.getExcelSheetData(downloadedFile.getAbsolutePath(),0,8,1);
-		System.out.println("hashMapExcelData == "+ hashMapExcelData);
-		System.out.println("has map keys  =="+hashMapExcelData.keySet().toString());
 		
 		String expectedColumnsInExportedExcel = "[Work Item: Work Item Number, Account #, Name, Roll Code, APN, Street, Use Code, Work Pool, Request Type, Status, Date, Value, Created In Roll Year, Remarks, Age(Days), Completed Date]";
-		softAssert.assertContains(hashMapExcelData.keySet().toString(),expectedColumnsInExportedExcel,"SMAB-T433: Columns Validation in downloaded building permit report");
-		//expectedColumnsInExportedExcel.toString();
-
+		softAssert.assertContains(hashMapExcelData.keySet().toString(),expectedColumnsInExportedExcel,"SMAB-T2520: Columns Validation in downloaded Executive All Work Items report");
+		
         //Logout of APAS Application
 		objApasGenericPage.logout();
     }
