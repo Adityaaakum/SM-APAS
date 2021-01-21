041109f0a5255af1480ec9555c508361d0318bbe
SPant <SPant@EC2AMAZ-V5BHSSR.smcare.org>
2021-01-21 22:04:10 +0530

    updating files

--- src/test/java/com/apas/Tests/SecurityAndSharing/WorkItems_SecurityAndSharing_Tests.java
+++ src/test/java/com/apas/Tests/SecurityAndSharing/WorkItems_SecurityAndSharing_Tests.java
@@ -38,35 +38,107 @@ public class WorkItems_SecurityAndSharing_Tests extends TestBase implements test
 	 */
 	@Test(description = "SMAB-T2033: verify that work pool supervisor is not able to mass transfer WI's with status 'submitted for approval', 'Approval-On Hold' and 'completed'", dataProvider = "loginRPBusinessAdmin", dataProviderClass = DataProviders.class, groups = {"regression","work_item_manual"  })
 	public void WorkItems_SharingAndSecurity_NoMassApproval(String loginUser) throws Exception {
-		
+
 		// Step1: Login to the APAS application using the credentials passed through dataprovider (RP Business Admin)
 		objWorkItemHomePage.login(loginUser);
-		 
+
 		//Step2: Navigate to home and submittedForApproval
 		objWorkItemHomePage.searchModule(HOME);
-	    objWorkItemHomePage.Click(objWorkItemHomePage.lnkTABMySubmittedforApproval);
-	    
-	    
-	    //Validate that work pool supervisor is not able to mass transfer WI's with status 'submitted for approval'
-	    softAssert.assertTrue(objLoginPage.waitForElementToBeInVisible(objWorkItemHomePage.changeWorkPool ,10), "SMAB-T2033: Validate that work pool supervisor is not able to mass transfer WI's with status 'submitted for approval'");
-	    softAssert.assertTrue(objLoginPage.waitForElementToBeInVisible(objWorkItemHomePage.changeAssignee,10), "SMAB-T2033: Validate that work pool supervisor is not able to mass transfer WI's with status 'submitted for approval'");
-	    
-	    //Validate that work pool supervisor is not able to mass transfer WI's with status 'Approval-On Hold'
+		objWorkItemHomePage.Click(objWorkItemHomePage.lnkTABMySubmittedforApproval);
+
+
+		//Validate that work pool supervisor is not able to mass transfer WI's with status 'submitted for approval'
+		softAssert.assertTrue(objLoginPage.waitForElementToBeInVisible(objWorkItemHomePage.changeWorkPool ,10), "SMAB-T2033: Validate that work pool supervisor is not able to mass transfer WI's with status 'submitted for approval'");
+		softAssert.assertTrue(objLoginPage.waitForElementToBeInVisible(objWorkItemHomePage.changeAssignee,10), "SMAB-T2033: Validate that work pool supervisor is not able to mass transfer WI's with status 'submitted for approval'");
+
+		//Validate that work pool supervisor is not able to mass transfer WI's with status 'Approval-On Hold'
 		objWorkItemHomePage.searchModule(HOME);
-	    driver.navigate().refresh();
-	    objWorkItemHomePage.waitForElementToBeVisible(objWorkItemHomePage.lnkTABMySubmittedforApproval);
+		driver.navigate().refresh();
+		objWorkItemHomePage.waitForElementToBeVisible(objWorkItemHomePage.lnkTABMySubmittedforApproval);
 		objWorkItemHomePage.openTab("On Hold");
-	    softAssert.assertTrue(objLoginPage.waitForElementToBeInVisible(objWorkItemHomePage.changeWorkPool ,10), "SMAB-T2033: Validate that work pool supervisor is not able to mass transfer WI's with status 'Approval-On Hold'");
-	    softAssert.assertTrue(objLoginPage.waitForElementToBeInVisible(objWorkItemHomePage.changeAssignee,10), "SMAB-T2033: Validate that work pool supervisor is not able to mass transfer WI's with status 'Approval-On Hold'");
-	    
-	    //Validate that work pool supervisor is not able to mass transfer WI's with status 'completed'
+		softAssert.assertTrue(objLoginPage.waitForElementToBeInVisible(objWorkItemHomePage.changeWorkPool ,10), "SMAB-T2033: Validate that work pool supervisor is not able to mass transfer WI's with status 'Approval-On Hold'");
+		softAssert.assertTrue(objLoginPage.waitForElementToBeInVisible(objWorkItemHomePage.changeAssignee,10), "SMAB-T2033: Validate that work pool supervisor is not able to mass transfer WI's with status 'Approval-On Hold'");
+
+		//Validate that work pool supervisor is not able to mass transfer WI's with status 'completed'
 		objWorkItemHomePage.searchModule(HOME);
-	    driver.navigate().refresh();
-	    objWorkItemHomePage.waitForElementToBeVisible(objWorkItemHomePage.lnkTABCompleted);
-	    objWorkItemHomePage.Click(objWorkItemHomePage.lnkTABCompleted);
-	    softAssert.assertTrue(objLoginPage.waitForElementToBeInVisible(objWorkItemHomePage.changeWorkPool ,10), "SMAB-T2033: Validate that work pool supervisor is not able to mass transfer WI's with status 'completed'");
-	    softAssert.assertTrue(objLoginPage.waitForElementToBeInVisible(objWorkItemHomePage.changeAssignee,10), "SMAB-T2033: Validate that work pool supervisor is not able to mass transfer WI's with status 'completed'");
+		driver.navigate().refresh();
+		objWorkItemHomePage.waitForElementToBeVisible(objWorkItemHomePage.lnkTABCompleted);
+		objWorkItemHomePage.Click(objWorkItemHomePage.lnkTABCompleted);
+		softAssert.assertTrue(objLoginPage.waitForElementToBeInVisible(objWorkItemHomePage.changeWorkPool ,10), "SMAB-T2033: Validate that work pool supervisor is not able to mass transfer WI's with status 'completed'");
+		softAssert.assertTrue(objLoginPage.waitForElementToBeInVisible(objWorkItemHomePage.changeAssignee,10), "SMAB-T2033: Validate that work pool supervisor is not able to mass transfer WI's with status 'completed'");
 
 		objWorkItemHomePage.logout();
 	}
+	/**
+	 * This method is to verify All the required button present on WI Home page For Supervisor User
+	 * 
+	 * @param loginUser
+	 * @throws Exception
+	 */
+	@Test(description = "SMAB-T2636: Work Input - verify All the required button present on WI Home page For Supervisor User", dataProvider = "loginRPBusinessAdmin", dataProviderClass = DataProviders.class, groups = {"regression","work_item_manual"  })
+	public void WorkItems_SharingAndSecurity_ButtonPresent_WIHomePage_SupervisorUser(String loginUser) throws Exception {
+
+		// Step1: Login to the APAS application using the credentials passed through dataprovider (RP Business Admin)
+		objWorkItemHomePage.login(loginUser);
+
+		//Step2: Navigate to home page
+		objWorkItemHomePage.searchModule(HOME);
+
+		//Step3: Validate that no  buttons in completed tab
+		objWorkItemHomePage.waitForElementToBeVisible(10,objWorkItemHomePage.lnkTABInProgress);
+		objWorkItemHomePage.openTab(objWorkItemHomePage.TAB_COMPLETED);
+		softAssert.assertTrue(objLoginPage.verifyElementNotVisible(objWorkItemHomePage.btnApprove), "SMAB-T2636: Validate that no button is visible in completed tab");
+		softAssert.assertTrue(objLoginPage.verifyElementNotVisible(objWorkItemHomePage.returnWorkItemButton), "SMAB-T2636: Validate that no button is visible in completed tab");
+		softAssert.assertTrue(objLoginPage.verifyElementNotVisible(objWorkItemHomePage.btnMarkComplete), "SMAB-T2636: Validate that no button is visible in completed tab");
+		softAssert.assertTrue(objLoginPage.verifyElementNotVisible(objWorkItemHomePage.acceptWorkItemButton), "SMAB-T2636: Validate that no button is visible in completed tab");
+
+		//Step4: Validate the buttons in Needs My Approval tab
+		objWorkItemHomePage.Click(objWorkItemHomePage.needsMyApprovalTab);
+		softAssert.assertTrue(objLoginPage.verifyElementVisible(objWorkItemHomePage.getButtonWithText(objWorkItemHomePage.reassignButton)), "SMAB-T2636: Validate that reassignButton is visible in Needs My Approval  tab");
+		softAssert.assertTrue(objLoginPage.verifyElementVisible(objWorkItemHomePage.btnApprove), "SMAB-T2636: Validate that btnApprove is visible in Needs My Approval  tab");
+		softAssert.assertTrue(objLoginPage.verifyElementVisible(objWorkItemHomePage.returnWorkItemButton), "SMAB-T2636: Validate that returnWorkItemButton is visible in Needs My Approval  tab");
+		softAssert.assertTrue(objLoginPage.verifyElementVisible(objWorkItemHomePage.getButtonWithText(objWorkItemHomePage.PutOnHoldButton)), "SMAB-T2636: Validate that PutOnHoldButton is visible in Needs My Approval  tab");
+		softAssert.assertTrue(objLoginPage.verifyElementVisible(objWorkItemHomePage.getButtonWithText(objWorkItemHomePage.assignLevel2Approver)), "SMAB-T2636: Validate that assignLevel2Approver is visible in Needs My Approval  tab");
+
+		//Step5: Validate the buttons in In Progress tab
+		objWorkItemHomePage.Click(objWorkItemHomePage.lnkTABInProgress);
+		softAssert.assertTrue(objLoginPage.verifyElementVisible(objWorkItemHomePage.btnMarkComplete), "SMAB-T2636: Validate that mark complete button is visible in InProgress tab");
+		softAssert.assertTrue(objLoginPage.verifyElementVisible(objWorkItemHomePage.getButtonWithText(objWorkItemHomePage.returnToPool)), "SMAB-T2636: Validate that returnToPool button is visible in InProgress tab");
+		//softAssert.assertTrue(objLoginPage.verifyElementVisible(objWorkItemHomePage.getButtonWithText(objWorkItemHomePage.PutOnHoldButton)), "SMAB-T2033: Validate that PutOnHoldButton button is visible in InProgress tab");
+		softAssert.assertTrue(objLoginPage.verifyElementVisible(objWorkItemHomePage.getButtonWithText(objWorkItemHomePage.ConsolidateButton)), "SMAB-T2636: Validate that ConsolidateButton button is visible in InProgress tab");
+
+		//Step6: Validate the buttons in In Pool tab
+		objWorkItemHomePage.Click(objWorkItemHomePage.lnkTABInPool);
+		softAssert.assertTrue(objLoginPage.verifyElementVisible(objWorkItemHomePage.acceptWorkItemButton), "SMAB-T2636: Validate that accept work item button is visible in InPool tab");
+
+		//Step7: Validate the buttons in On Hold tab
+		objWorkItemHomePage.openTab(objWorkItemHomePage.TAB_On_Hold);
+		softAssert.assertTrue(objLoginPage.verifyElementVisible(objWorkItemHomePage.getButtonWithText(objWorkItemHomePage.markInProgress)), "SMAB-T2636: Validate that mark in progress button is visible in On_Hold tab");
+
+		//Step8: Validate the buttons in SubmittedforApproval tab
+		objWorkItemHomePage.Click(objWorkItemHomePage.lnkTABMySubmittedforApproval);
+		softAssert.assertTrue(objLoginPage.verifyElementVisible(objWorkItemHomePage.getButtonWithText(objWorkItemHomePage.WithdrawButton)), "SMAB-T2636: Validate that WithdrawButton is present in SubmittedforApproval tab");
+
+		//Step9: Validate the buttons in StaffInProgress tab
+		objWorkItemHomePage.openTab(objWorkItemHomePage.TAB_StaffInProgress);
+		softAssert.assertTrue(objLoginPage.verifyElementVisible(objWorkItemHomePage.getButtonWithText(objWorkItemHomePage.changeWorkPool)), "SMAB-T2636: Validate that changeWorkPool button is present in StaffInProgress tab");
+		softAssert.assertTrue(objLoginPage.verifyElementVisible(objWorkItemHomePage.getButtonWithText(objWorkItemHomePage.changeAssignee)), "SMAB-T2636: Validate that changeAssignee button is present in StaffInProgress tab");
+
+		//Step10: Validate the buttons in StaffOnHold tab
+		driver.navigate().refresh();
+		objWorkItemHomePage.waitForElementToBeVisible(10,objWorkItemHomePage.lnkTABInProgress);
+		objWorkItemHomePage.openTab(objWorkItemHomePage.TAB_StaffOnHold);
+		softAssert.assertTrue(objLoginPage.verifyElementVisible(objWorkItemHomePage.getButtonWithText(objWorkItemHomePage.changeWorkPool)), "SMAB-T2636: Validate that changeWorkPool button is present in Staff on hold tab");
+		softAssert.assertTrue(objLoginPage.verifyElementVisible(objWorkItemHomePage.getButtonWithText(objWorkItemHomePage.changeAssignee)), "SMAB-T2636: Validate that changeAssignee button is present in Staff on hold tab");
+
+		//Step11: Validate the buttons in StaffInPool tab
+		driver.navigate().refresh();
+		objWorkItemHomePage.waitForElementToBeVisible(10,objWorkItemHomePage.lnkTABInProgress);
+		objWorkItemHomePage.openTab(objWorkItemHomePage.TAB_StaffInPool);
+		softAssert.assertTrue(objLoginPage.verifyElementVisible(objWorkItemHomePage.getButtonWithText(objWorkItemHomePage.changeWorkPool)), "SMAB-T2636: Validate that changeWorkPool button is present in StaffInPool tab");
+		softAssert.assertTrue(objLoginPage.verifyElementVisible(objWorkItemHomePage.getButtonWithText(objWorkItemHomePage.changeAssignee)), "SMAB-T2636: Validate that changeAssignee button is present in StaffInPool tab");
+
+		objWorkItemHomePage.logout();
+	}
+
 }
