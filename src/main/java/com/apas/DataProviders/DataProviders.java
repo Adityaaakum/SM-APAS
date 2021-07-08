379e16071cc7e4be98591cce82b42a7ab8148729
SPant <SPant@WSAMZN-0D4SC44B.smcare.org>
2021-07-08 19:53:51 +0530

    adding more webelements for CIO transfer page

--- src/main/java/com/apas/DataProviders/DataProviders.java
+++ src/main/java/com/apas/DataProviders/DataProviders.java
@@ -399,8 +399,8 @@ public class DataProviders {
 	 * Below function will be used to login to application with different users
 	 * @returns: Return the user CIO Staff
 	 **/
-    @DataProvider(name = "loginCIOStaff")
-    public Object[][] dpCIOStaffUser() {
-        return new Object[][] { { users.CIO_STAFF } };
+    @DataProvider(name = "loginCIOStaffUser")
+    public Object[][] CIOStaffUser() {
+        return new Object[][] {{ users.CIO_STAFF }};
     }
 }
