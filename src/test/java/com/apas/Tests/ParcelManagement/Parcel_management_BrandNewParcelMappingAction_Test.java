efca9472862f69435c5c4675324d859016add457
Aaishwarya <Aaishwarya@WSAMZN-PNJPG9AB.smcare.org>
2021-10-21 07:04:36 +0000

    fix

--- src/test/java/com/apas/Tests/ParcelManagement/Parcel_management_BrandNewParcelMappingAction_Test.java
+++ src/test/java/com/apas/Tests/ParcelManagement/Parcel_management_BrandNewParcelMappingAction_Test.java
@@ -425,41 +425,41 @@ public class Parcel_management_BrandNewParcelMappingAction_Test extends TestBase
 		    objMappingPage.logout();
 		    Thread.sleep(3000);
 		    
-//			ReportLogger.INFO(" Appraiser logins ");
-//			objMappingPage.login(users.RP_APPRAISER);
-//			objMappingPage.searchModule(PARCELS);
-//			objMappingPage.globalSearchRecords(newCreatedApn);
-//			objParcelsPage.Click(objParcelsPage.workItems);
-//			
-//			//Moving to the Update Characteristics Verify PUC WI
-//			objParcelsPage.Click(objParcelsPage.updateCharacteristicsVerifyPUC);
-//			objWorkItemHomePage.clickOnTimelineAndMarkComplete(objWorkItemHomePage.inProgressOptionInTimeline);
-//			objWorkItemHomePage.Click(objWorkItemHomePage.detailsTab);
-//			objWorkItemHomePage.waitForElementToBeVisible(40, objWorkItemHomePage.referenceDetailsLabel);
-//			objWorkItemHomePage.Click(objWorkItemHomePage.reviewLink);
-//			objWorkItemHomePage.switchToNewWindow(parentWindow);
-//			objParcelsPage.Click(objParcelsPage.getButtonWithText("Done"));
-//			ReportLogger.INFO("Update Characteristics Verify PUC WI Completed");
-//
-//			driver.switchTo().window(parentWindow);
-//			driver.navigate().refresh();
-//			objWorkItemHomePage.waitForElementToBeVisible(objWorkItemHomePage.submittedforApprovalTimeline);
-//			objWorkItemHomePage.Click(objWorkItemHomePage.detailsTab);
-//			String workItemStatus = objMappingPage.getFieldValueFromAPAS("Status", "Information");
-//			softAssert.assertEquals(workItemStatus, "Completed", "SMAB-T3771: Validation WI completed successfully");
-//			objMappingPage.searchModule(PARCELS);
-//			objMappingPage.globalSearchRecords(newCreatedApn);
-//			
-//			//Moving to Allocate Values WI
-//			objParcelsPage.Click(objParcelsPage.workItems);
-//			objParcelsPage.Click(objParcelsPage.allocateValue);
-//			objWorkItemHomePage.Click(objWorkItemHomePage.detailsTab);
-//			String assignedTo = objMappingPage.getFieldValueFromAPAS("Assigned To", "Information");
-//			String workPool = objMappingPage.getFieldValueFromAPAS("Work Pool", "Information");
-//			softAssert.assertEquals(assignedTo, salesforceAPI.select("SELECT Name FROM User where Username ='" + objMappingPage.userNameForRpAppraiser + "'").get("Name").get(0), "SMAB-T3771:Assiged to is matched successfully");
-//			softAssert.assertEquals(workPool, objMappingPage.appraiserwWorkPool, "SMAB-T3771:workPool is matched successfully");
-//
-//			objWorkItemHomePage.logout();
+			ReportLogger.INFO(" Appraiser logins ");
+			objMappingPage.login(users.RP_APPRAISER);
+			objMappingPage.searchModule(PARCELS);
+			objMappingPage.globalSearchRecords(newCreatedApn);
+			objParcelsPage.Click(objParcelsPage.workItems);
+			
+			//Moving to the Update Characteristics Verify PUC WI
+			objParcelsPage.Click(objParcelsPage.updateCharacteristicsVerifyPUC);
+			objWorkItemHomePage.clickOnTimelineAndMarkComplete(objWorkItemHomePage.inProgressOptionInTimeline);
+			objWorkItemHomePage.Click(objWorkItemHomePage.detailsTab);
+			objWorkItemHomePage.waitForElementToBeVisible(40, objWorkItemHomePage.referenceDetailsLabel);
+			objWorkItemHomePage.Click(objWorkItemHomePage.reviewLink);
+			objWorkItemHomePage.switchToNewWindow(parentWindow);
+			objParcelsPage.Click(objParcelsPage.getButtonWithText("Done"));
+			ReportLogger.INFO("Update Characteristics Verify PUC WI Completed");
+
+			driver.switchTo().window(parentWindow);
+			driver.navigate().refresh();
+			objWorkItemHomePage.waitForElementToBeVisible(objWorkItemHomePage.submittedforApprovalTimeline);
+			objWorkItemHomePage.Click(objWorkItemHomePage.detailsTab);
+			String workItemStatus = objMappingPage.getFieldValueFromAPAS("Status", "Information");
+			softAssert.assertEquals(workItemStatus, "Completed", "SMAB-T3771: Validation WI completed successfully");
+			objMappingPage.searchModule(PARCELS);
+			objMappingPage.globalSearchRecords(newCreatedApn);
+			
+			//Moving to Allocate Values WI
+			objParcelsPage.Click(objParcelsPage.workItems);
+			objParcelsPage.Click(objParcelsPage.allocateValue);
+			objWorkItemHomePage.Click(objWorkItemHomePage.detailsTab);
+			String assignedTo = objMappingPage.getFieldValueFromAPAS("Assigned To", "Information");
+			String workPool = objMappingPage.getFieldValueFromAPAS("Work Pool", "Information");
+			softAssert.assertEquals(assignedTo, salesforceAPI.select("SELECT Name FROM User where Username ='" + objMappingPage.userNameForRpAppraiser + "'").get("Name").get(0), "SMAB-T3771:Assiged to is matched successfully");
+			softAssert.assertEquals(workPool, objMappingPage.appraiserwWorkPool, "SMAB-T3771:workPool is matched successfully");
+
+			objWorkItemHomePage.logout();
 
 		   		
             		                          
