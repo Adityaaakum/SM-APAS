79893077e82bff7a7c4e0daf0d98bdb98f98843e
SPant <SPant@WSAMZN-0D4SC44B.smcare.org>
2022-01-11 09:45:09 +0530

    bpp scripts maintenance

--- src/main/java/com/apas/PageObjects/Page.java
+++ src/main/java/com/apas/PageObjects/Page.java
@@ -737,7 +737,8 @@ public class Page extends TestBase {
 				commonPath + "//label[text()=\"" + label + "\"]//parent::div//div//a | " + //this condition was observed on Mapping Action screen for Assessor' Map label
 				commonPath + "//label[text()=\"" + label + "\"]//parent::div//div//span[@class='slds-col'] | " + //this condition was observed on Mapping Action screen for Parent APN(s) field
 				commonPath + "//label[text()=\"" + label + "\"]/..//textarea | "+//this condition was added to handle webelements of type textarea
-				commonPath + "//span[text()=\"" + label + "\"]/../following-sibling::input | "+					
+				commonPath + "//span[text()=\"" + label + "\"]/../following-sibling::input | "+	
+				commonPath + "//label[text()=\"" + label + "\"]//following::div[@class='slds-form-element__control'] | "+
 				commonPath+"//a[@title=\""+label+"\"]";
 			
 		waitUntilElementIsPresent(xpath, 10);
