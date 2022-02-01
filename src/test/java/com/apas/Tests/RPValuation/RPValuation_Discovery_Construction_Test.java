f39045199b58b98b95403c445003fbbfcd566189
Chirag Arora <CArora@smcgov.org>
2022-02-01 14:46:04 +0530

    Cleanup in terms of Groups and Components

--- src/test/java/com/apas/Tests/RPValuation/RPValuation_Discovery_Construction_Test.java
+++ src/test/java/com/apas/Tests/RPValuation/RPValuation_Discovery_Construction_Test.java
@@ -25,7 +25,7 @@ import com.apas.Utils.Util;
 import com.apas.config.modules;
 import com.apas.config.testdata;
 
-public class RPValuation_WorkItemFlow extends TestBase {
+public class RPValuation_Discovery_Construction_Test extends TestBase {
 	
 	private RemoteWebDriver driver;
 	Page objPage;
@@ -74,8 +74,8 @@ public class RPValuation_WorkItemFlow extends TestBase {
 			+ " list and its Type label should be NC, the Actions label should be Construction - Other", 
 			dataProvider = "RPAppraiser", 
 			dataProviderClass = DataProviders.class , 
-			groups = {"Smoke","Regression","RP_VAL","WorkItemWorkflow_RP_VAL_Construction"})
-	public void WorkItemWorkflow_RP_VAL_ConstructionManualWICreation(String loginUser) throws Exception {
+			groups = {"Regression","RPValuation","WorkItemWorkflow","Construction", "DiscoveryConstruction", "BuildingPermit"})
+	public void BuildingPermit_Manual_Discovery_Construction_WorkItem(String loginUser) throws Exception {
 		
 		//Fetching parcel that are Active
 		String queryApnDetails ="SELECT Id,Name FROM Parcel__c where primary_situs__c != NULL and "
@@ -170,120 +170,12 @@ public class RPValuation_WorkItemFlow extends TestBase {
 	    	
 	    	softAssert.assertTrue(flag, "SMAB-T4232:" + newConstructionPage);
 	    }
+	 
+	    // Logout at the end of the test
+	    objWIHomePage.logout();
 		
 	}
 	
-	/*
-	 * This method is to create a WI for the Demolition Discovery by the Appraiser
-	 * @param loginUser
-	 * @throws Exception
-	 */
-
-	@Test(description = "SMAB-T4272,SMAB-T4273,SMAB-T4274:RP Demolition Discovery- Verify that Appraisers "
-			+ "will have the ability to create a demolition discovery work item from the Component Actions"
-			+ " list and its Type label should be NC, the Actions label should be Demo - Other", 
-			dataProvider = "RPAppraiser", 
-			dataProviderClass = DataProviders.class , 
-			groups = {"Smoke","Regression","RP_VAL","WorkItemWorkflow_RP_VAL_Demolition"})
-	public void WorkItemWorkflow_RP_VAL_DemolitionManualWICreation(String loginUser) throws Exception {
-		
-		//Fetching parcel that are Active
-		String queryApnDetails ="SELECT Id,Name FROM Parcel__c where primary_situs__c != NULL and "
-				+ "Status__c='Active' and Id NOT IN (SELECT APN__c FROM Work_Item__c where "
-				+ "type__c='CIO') and (Not Name like '100%') and (Not Name like '800%') "
-				+ "and (Not Name like '%990') and (Not Name like '134%') Limit 2";
-		
-		HashMap<String, ArrayList<String>> responseAPNDetails = salesforceAPI.select(queryApnDetails);
-		String apn1=responseAPNDetails.get("Name").get(0);		
-		String apnId1=responseAPNDetails.get("Id").get(0);
-				
-		
-		Map<String, String> newConstructionWIData = objUtil.generateMapFromJsonFile(manualWIFilePath, 
-				                               "DataToCreateWorkItemOfTypeNCWithActionDemolitionOther");
-		
-		//Step1: Login to the APAS application using the credentials passed through data provider (Business admin or appraisal support)
-		ReportLogger.INFO("Step 1: Login to the Salesforce ");
-		objApasGenericPage.login(loginUser);
-		
-		
-		objApasGenericPage.searchModule(modules.APAS);
-		
-		// Step2: Navigating to the Parcel View page								
-		String execEnv = System.getProperty("region");
-
-		driver.navigate().to("https://smcacre--" + execEnv + ".lightning.force.com"
-				+ "/lightning/r/Parcel__c/"
-				+ apnId1 + "/view");
-				
-		// Step 3: Creating Manual work item for the Parcel 
-		String WINumber = objParcel.createWorkItem(newConstructionWIData);
-		
-		if(!WINumber.isEmpty()) {
-						
-			softAssert.assertTrue(true, "SMAB-T4272: Demolition Discovery WI is created successfully");
-		}else {
-			
-			softAssert.assertTrue(false, "SMAB-T4272: Demolition Discovery WI is created successfully");
-		}
-						
-		
-		//Step 4:Clicking the  details tab for the work item newly created and clicking on Related Action Link
-		objWIHomePage.Click(objWIHomePage.detailsTab);
 	
-		String sqlWIDetails = "select type__c ,sub_type__c ,"
-				+ "use_code_f__c , event_id__c ,request_type__c "
-				+ "from work_item__c where name = '"+WINumber+"'";
-				
-		HashMap<String, ArrayList<String>> responseWIDetails= salesforceAPI.select(sqlWIDetails);	
-		
-		String expectedType = responseWIDetails.get("Type__c").get(0); 
-		String expectedAction = responseWIDetails.get("Sub_Type__c").get(0);
-		String expectedReference = responseWIDetails.get("Event_ID__c").get(0);
-		String expectedRequestType = responseWIDetails.get("Request_Type__c").get(0);
-		String expectedRelatedAction = responseWIDetails.get("Sub_Type__c").get(0);
-		String expectedEventID = responseWIDetails.get("Event_ID__c").get(0);
-		String expectedUseCode = responseWIDetails.get("Use_Code_f__c").get(0);
-		
-		
-		//verify the WI details		
-		String actualType = objWIHomePage.getFieldValueFromAPAS("Type"); 
-		String actualAction = objWIHomePage.getFieldValueFromAPAS("Action");;
-		String actualReference = objWIHomePage.getFieldValueFromAPAS("Reference");;
-		String actualRequestType = objWIHomePage.getFieldValueFromAPAS("Request Type");;
-		String actualRelatedAction = objWIHomePage.getFieldValueFromAPAS("Related Action");;
-		String actualAPN = objWIHomePage.getFieldValueFromAPAS("APN");
-		String actualEventID = objWIHomePage.getFieldValueFromAPAS("Event ID");
-		String actualUseCode = objWIHomePage.getFieldValueFromAPAS("Use Code");
-		
-		//Asserrtions
-		softAssert.assertEquals(actualType, expectedType, "SMAB-T4273: The WI Type is verified");
-		softAssert.assertEquals(actualAction, expectedAction, "SMAB-T4273: The WI Action is verified");
-		softAssert.assertEquals(actualReference, expectedReference, "SMAB-T4273: The WI Reference is verified");
-		softAssert.assertEquals(actualRequestType, expectedRequestType, "SMAB-T4273: The WI RequestType is verified");
-		softAssert.assertEquals(actualRelatedAction, expectedRelatedAction, "SMAB-T4273: The WI Related Action is verified");
-		softAssert.assertEquals(actualAPN, apn1, "SMAB-T4273: The WI APN is verified");
-		softAssert.assertEquals(actualEventID, expectedEventID, "SMAB-T4273: The WI Event ID is verified");
-		softAssert.assertEquals(actualUseCode, expectedUseCode, "SMAB-T4273: The WI Use Code is verified");
-		
-		objWIHomePage.Click(objWIHomePage.reviewLink);
-		
-		String parentWindow = driver.getWindowHandle();
-		ReportLogger.INFO("Switch to the Mapping Action screen");
-		objWIHomePage.switchToNewWindow(parentWindow);
-		
-		String newConstructionPage = "As part of the new construction process, confirm and update the values as needed:";		
-		
-		String  constructPage = driver.findElement(By.xpath("//div[@class='slds-rich-text-editor__output uiOutputRichText forceOutputRichText']/p/span")).getText();
-		
-		boolean flag= false;
-	    if(constructPage.equalsIgnoreCase(newConstructionPage)) {
-	    	
-	    	flag = true;
-	    	
-	    	softAssert.assertTrue(flag, "SMAB-T4274:" + newConstructionPage);
-	    }	
-		
-		
-	}
 
 }
